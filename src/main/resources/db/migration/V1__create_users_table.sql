69cf6aa24f30c7498d6993592276936c861484f5
Max <maksym.sheremet@outlook.com>
2026-02-12 17:28:34 +0200

    12_Flyway_міграція

--- src/main/resources/db/migration/V1__create_users_table.sql
+++ src/main/resources/db/migration/V1__create_users_table.sql
@@ -1,15 +1,14 @@
 CREATE TABLE users
 (
-    id        BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    firstname VARCHAR(255) NOT NULL,
-    lastname  VARCHAR(255),
-    email     VARCHAR(255) NOT NULL UNIQUE,
-    password  VARCHAR(255),
-    provider  VARCHAR(50)  NOT NULL,
-    provider_id VARCHAR(255) NOT NULL,
-    enabled   BOOLEAN      NOT NULL DEFAULT TRUE,
-    role      VARCHAR(50)  NOT NULL,
-    CONSTRAINT unique_provider_id UNIQUE (provider_id)
+    id          BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    firstname   VARCHAR(255) NOT NULL,
+    lastname    VARCHAR(255),
+    email       VARCHAR(255) NOT NULL UNIQUE,
+    password    VARCHAR(255),
+    provider    VARCHAR(50)  NOT NULL,
+    provider_id VARCHAR(255) NOT NULL UNIQUE,
+    enabled     BOOLEAN      NOT NULL DEFAULT TRUE,
+    role        VARCHAR(50)  NOT NULL
 );
 
 CREATE INDEX idx_users_provider_provider_id ON users (provider, provider_id);
